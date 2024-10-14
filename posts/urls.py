0a55a68c7bf51ea5f0eb2d26ac039ca976862af8
Kent Espia <151008963+Aipse-bot@users.noreply.github.com>
2024-10-14 20:26:19 +0800

    Revert "Added Communication Branch"

--- posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.Posts, name='post'),
-    path('create_post', views.Create_Post, name='create_post'),
-    path('post_list', views.Post_list, name='post_list')
-
-]
