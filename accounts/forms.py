fe25381bd83a8c2b6c2dc6255085ee070c0050ec
Kaazzz <zakfloreta20@gmail.com>
2024-10-16 18:42:36 +0800

    Added accounts functionality as well as login and register feature

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,19 +3,29 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
+    
+    first_name = forms.CharField(max_length=30, required=True, label="First Name")
+    last_name = forms.CharField(max_length=30, required=True, label="Last Name")
+    birthday = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}), required=True, label="Birthday")
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'birthday', 'password1', 'password2']
         labels = {
             'username': 'Username',
             'email': 'Email',
             'password1': 'Password',
-            'password2': 'Confirm Password'
+            'password2': 'Confirm Password',
+            'birthday': 'Birthday'
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        
         self.fields['username'].help_text = None
         self.fields['password1'].help_text = None
         self.fields['password2'].help_text = None
         self.fields['email'].help_text = None
+        self.fields['first_name'].help_text = None
+        self.fields['last_name'].help_text = None
+        self.fields['birthday'].help_text = None
--- studyhive/settings.py
+++ studyhive/settings.py
@@ -124,3 +124,6 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.fallback.FallbackStorage'
