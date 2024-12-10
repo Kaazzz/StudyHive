9c5952f72c93b751fd03228ec4f1d50af506c2e2
Kent Espia <kentvirgil.espia@cit.edu>
2024-12-10 21:16:19 +0800

    fix calendar Urls

--- calendar_scheduler/urls.py
+++ calendar_scheduler/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
     path('create_event', views.create_event, name='create_event'),
     path('delete_event/<int:event_id>/', views.delete_event, name='delete_event'),
-    path('upcoming_event/', views.upcoming_event_list, name='upcoming_event'),
-    path('', views.calendar_management, name='calendar_management'),
-    path('attended_events/', views.attended_event_list, name='attended_event'),   
+    path('', views.calendar_management, name='calendar_management'), 
     path('edit/<int:event_id>/', views.edit_event, name='edit_event'),
 ]
